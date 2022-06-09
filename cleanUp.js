5aba77053ce69d1896eba9d2d4d17a0c98bac53c
Colin Ciervo <colinciervo@@gmail.com>
2022-06-09 15:17:35 -0400

    Feeling good

--- cleanUp.js
+++ cleanUp.js
@@ -3,44 +3,41 @@
 // YOUR TASK:
   // Modify the code to make that improvement(s)
   // write a JS comment to explain what you changed, and why
-  // make sure the code you submit WORKS - you can run this entire file 
+  // make sure the code you submit WORKS - you can run this entire file
     // using `node cleanUp.js` or copy and paste into a replit
 
 
-// EX 1:
-function nameQuestion() {
-  console.log("Hello, what is your name?")
-}
+    // EX 1:
 
-nameQuestion()
+      console.log("Hello, what is your name?")
+  //having function there added nothing to the code.
+
+    // EX 2:
+    function addThreeNums(first, second, third) {
+    var sum = first + second + third
+    console.log(sum);
+          }
+
+    addThreeNums(1, 2, 3);
+    addThreeNums(4, 2, 7);
+//This one looks ok to me
+
+    // EX 3:
+    function makeFreshPesto(){
+      console.log("Buy ingredients: basil, parmesan, romano, olive oil, pine nuts, garlic, salt, pepper");
+      console.log("Pulse basil and pine nuts");
+      console.log("Add garlic and cheeses");
+      console.log("Slowly pour in oil");
+      console.log("Season");    }
+
+    makeFreshPesto();
 
 
-// EX 2:
-function addThreeNums(first, second, third) {
-var sum = first + second + third
-console.log(sum);
+    //  EX 4:
+    function average(num1, num2)
+      {
+    var sum = num1 + num2;
+        var avg = sum / 2;
+
+      console.log(`the average is: ${avg}.`)
       }
-
-addThreeNums(1, 2, 3);
-addThreeNums(4, 2, 7);
-
-
-// EX 3:
-func makeFreshPesto(){
-  console.log("Buy ingredients: basil, parmesan, romano, olive oil, pine nuts, garlic, salt, pepper");
-  console.log("Pulse basil and pine nuts");
-  console.log("Add garlic and cheeses");
-  console.log("Slowly pour in oil");
-  console.log("Season");    }
-
-makeFreshPesto();
-
-
-//  EX 4:
-function average(num1, num2) 
-  {
-var sum = num1 + num2;
-    var avg = sum / 2;
-
-  console.log(`the average is: ${avg}`.)
-  }
--- functions.js
+++ functions.js
@@ -1,11 +1,25 @@
 // 1: Write a function named printGreeting that prints a simple greeting message, the same one, every time it is called. Call this function 3 times.
+function printGreeting() {
+  console.log("Hello, User")
+}
 
+printGreeting()
+printGreeting()
+printGreeting()
 
 // 2: Write a function that accepts 1 argument. The function should console.log a sentence that interpolates the data passed in.
+function printGreeting(name) {
+  console.log(`Hello, ${name}`);
+}
 
+printGreeting("Colin")
 
 // 3: Write a function that has 3 parameters: a string and two numbers. The String will be the name of a company, and the numbers will represent the minimum and maximum of a pay range for a posted job. The function should print out a sentence that includes the name of the company and the range itself (if the numbers passed in are 90000 and 110000, the pay range is 20000).
+function salaryRange(place, min, max, range) {
+  console.log(`Here at ${place} you can make between ${min} and ${max}. Because we hate you. Thats a total range of ${range}!`);
+}
 
+salaryRange("Best Buy", "$10", "$11", "1");
 
 // 4: Write a function that satifies the following interaction pattern:
 
@@ -20,3 +34,27 @@ checkStock(0, "Cheese");
 
 checkStock(1, "Salsa");
 // => "Salsa - running LOW"
+
+function checkStock(amount, name) {
+  console.log(`Item: ${name}     Amount: ${amount}`)
+if (`${amount}` <= 3)
+  console.log(`${name} - running LOW`)
+  else {
+    console.log(`${name} is stocked`)
+  }
+if (`${amount}` == 0) {
+  console.log(`${name} - OUT of stock`)
+ }
+}
+
+checkStock(4, "Coffee");
+// => "Coffee is stocked"
+
+checkStock(3, "Tortillas");
+// => "Tortillas - running LOW"
+
+checkStock(0, "Cheese");
+// => "Cheese - OUT of stock!"
+
+checkStock(1, "Salsa");
+// => "Salsa - running LOW
